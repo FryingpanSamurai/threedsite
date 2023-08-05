afbaa61cb4581f0e99b2809a2e5b2e339b5a8d3b
bpatricka <bpatricka@live.com>
2023-08-04 23:34:58 -0600

    three

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+</svelte:head>
+
+<script>
+    import {onMount} from 'svelte';
+    import * as THREE from 'three';
+
+    /** @type {HTMLCanvasElement} */
+    let canvas;
+
+    onMount(() => {
+        const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
+
+        const fov = 75;
+        const aspect = 2;
+        const near = 0.1;
+        const far = 5;
+        const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+        camera.position.z = 2;
+
+        const scene = new THREE.Scene();
+        const boxW = 1;
+        const boxH = 1;
+        const boxD = 1;
+        const geometry = new THREE.BoxGeometry(boxW, boxH, boxD);
+        const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+        let time = new Date().getMilliseconds();
+        function render(time) {
+            time *= 0.001; //convert time to secs
+            
+            cube.rotation.x = time;
+            cube.rotation.y = time;
+
+            renderer.render(scene, camera);
+
+            requestAnimationFrame(render);
+        }
+        requestAnimationFrame(render);
+    })
+</script>
+
+<style>
+    :global(*) {
+        padding: 0;
+        margin: 0;
+    }
+    
+    .main{
+        width: 100vw;
+        height: 100vh;
+    }
+</style>
+
+<div class="main">
+    <canvas bind:this={canvas}></canvas>
+</div>
